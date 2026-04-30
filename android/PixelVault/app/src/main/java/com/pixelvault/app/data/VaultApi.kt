b77914f6c72473e1b659bd1651b57c70f63bea80
MenaceHecker <thedynamic2508@gmail.com>
2026-04-29 20:39:57 -0400

    Defined get, post api calls

--- android/PixelVault/app/src/main/java/com/pixelvault/app/data/VaultApi.kt
+++ android/PixelVault/app/src/main/java/com/pixelvault/app/data/VaultApi.kt
@@ -1,4 +1,17 @@
 package com.pixelvault.app.data
 
-class VaultApi {
+import com.pixelvault.app.data.model.ConfirmRequest
+import com.pixelvault.app.data.model.ConfirmResponse
+import com.pixelvault.app.data.model.PendingResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface VaultApi {
+
+    @GET("api/pending")
+    suspend fun getPending(): PendingResponse
+
+    @POST("api/confirm")
+    suspend fun confirm(@Body request: ConfirmRequest): ConfirmResponse
 }
