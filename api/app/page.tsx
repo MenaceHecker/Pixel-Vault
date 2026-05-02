7c56b1f55a4440cffd0e14b5fa03e5e2b49fe9c1
MenaceHecker <thedynamic2508@gmail.com>
2026-05-01 23:37:18 -0400

    Updated local properties

--- api/app/page.tsx
+++ api/app/page.tsx
@@ -1,4 +1,5 @@
 import { Redis } from "@upstash/redis";
+export const dynamic = "force-dynamic";
 const kv = new Redis({
   url: process.env.KV_REST_API_URL!,
   token: process.env.KV_REST_API_TOKEN!,
